78cd9a5419ddcbe6f3636fb120477a272acbf9d8
Monickss <monica15shift2@gmail.com>
2025-03-17 15:11:35 +0530

    Revert "commit"
    This reverts commit 4edfd2694d453099f0aa33205e7159dd6743b4b0.

--- resources/views/includes/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<!-- resources/views/includes/footer.blade.php -->
-<footer class="footer text-center text-muted">
-    &copy; {{ date('Y') }} - Hostel Management System - Developed by <a href="https://codeastro.com">CodeAstro</a>
-</footer>
